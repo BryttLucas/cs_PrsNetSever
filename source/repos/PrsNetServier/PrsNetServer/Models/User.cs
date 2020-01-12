465383776e7663abda4580a72dadf7cff466d048
BryttLucas <workingB@fuse.net>
2020-01-11 21:00:41 -0500

    Net Prs

---
+++ source/repos/PrsNetServier/PrsNetServer/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrsNetServer.Models {
+    public class User {
+        public int Id { get; set; }
+        [StringLength(30)]
+        [Required]
+        public string Username { get; set; }
+        [StringLength(30)]
+        [Required]
+        public string Password { get; set; }
+        [StringLength(30)]
+        public string Firstname { get; set; }
+        [StringLength(30)]
+        public string Lastname { get; set; }
+        [StringLength(12)]
+        public string Phone { get; set; }
+        [StringLength(50)]
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; } = false;
+        public bool IsReviewer { get; set; } = false;
+
+        public User() {
+
+        }
+
+    }
+}
